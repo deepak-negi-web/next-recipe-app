8d7f864847efd51dfa95ea747edbb4b023e3b76b
Deepak Negi <st.deepak15@gmail.com>
2021-08-29 14:37:00 +0530

    feat: data fetching implementation using getStaticProps

--- pages/recipes/index.jsx
+++ pages/recipes/index.jsx
@@ -1,25 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import BannerHeader from "../../Components/UI/Banner/BannerHeader/BannerHeader";
 import axios from "../../axios-post";
-import Spinner from "../../Components/UI/Spinner/Spinner";
 import { SEO } from "../../Components";
 
-const Menu = () => {
-  const [recipes, setRecipes] = useState(null);
+const Menu = ({ recipes = [] }) => {
   const router = useRouter();
-  useEffect(() => {
-    axios.get("/recipe.json").then((res) => {
-      const fetchedRecipes = [];
-      for (let key in res.data) {
-        fetchedRecipes.push({
-          id: key,
-          data: res.data[key],
-        });
-      }
-      setRecipes(fetchedRecipes);
-    });
-  }, []);
 
   const seeFullRecipe = (id) => {
     window.scrollTo(0, 0);
@@ -32,53 +18,13 @@ const Menu = () => {
       <br className="d-none d-xl-block" /> laughter and a heap of love
     </p>
   );
-  let menu = <Spinner />;
-  if (recipes) {
-    menu = (
-      <React.Fragment>
-        <BannerHeader pageName="Food Menu" pageDescription={Description} />
-        <section className="section-margin">
-          <div className="container">
-            <div className="section-intro mb-75px">
-              <h4 className="intro-title">Food Recipe Menu</h4>
-              <h2>Delicious food</h2>
-            </div>
-            <div className="row">
-              {recipes.map((recipe) => {
-                return (
-                  <div className="col-lg-6" key={recipe["id"]}>
-                    <div className="media align-items-center food-card">
-                      <img
-                        className="mr-3 mr-sm-4"
-                        src={recipe["data"]["imgURL"]}
-                        alt=""
-                        style={{ width: "99px", height: "99px" }}
-                      />
-                      <div className="media-body">
-                        <div className="d-flex justify-content-between">
-                          <h4>{recipe["data"]["dishname"]}</h4>
-                        </div>
-                        <p>
-                          {recipe["data"]["description"].substring(0, 25)}...
-                        </p>
-                        <button
-                          type="button"
-                          onClick={() => seeFullRecipe(recipe["id"])}
-                          className="btn btn-primary"
-                          name="button"
-                        >
-                          See Recipe
-                        </button>
-                        <input type="hidden" name="query" value="Pizza" />
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </section>
-      </React.Fragment>
+
+  if (recipes.length === 0) {
+    return (
+      <BannerHeader
+        pageName="Sorry!!"
+        pageDescription="No Recipes Available yet :("
+      />
     );
   }
 
@@ -89,9 +35,66 @@ const Menu = () => {
         desc="Nuskha brings the foods the most precious things,Enjoy the delicious food recipe"
         image="https://firebasestorage.googleapis.com/v0/b/nuskha-your-own-recipe.appspot.com/o/recipes.png?alt=media&token=5243713d-ee11-4a06-8945-c42ad3a58b30"
       />
-      {menu}
+      <BannerHeader pageName="Food Menu" pageDescription={Description} />
+      <section className="section-margin">
+        <div className="container">
+          <div className="section-intro mb-75px">
+            <h4 className="intro-title">Food Recipe Menu</h4>
+            <h2>Delicious food</h2>
+          </div>
+          <div className="row">
+            {recipes.map((recipe) => {
+              return (
+                <div className="col-lg-6" key={recipe["id"]}>
+                  <div className="media align-items-center food-card">
+                    <img
+                      className="mr-3 mr-sm-4"
+                      src={recipe["data"]["imgURL"]}
+                      alt=""
+                      style={{ width: "99px", height: "99px" }}
+                    />
+                    <div className="media-body">
+                      <div className="d-flex justify-content-between">
+                        <h4>{recipe["data"]["dishname"]}</h4>
+                      </div>
+                      <p>{recipe["data"]["description"].substring(0, 25)}...</p>
+                      <button
+                        type="button"
+                        onClick={() => seeFullRecipe(recipe["id"])}
+                        className="btn btn-primary"
+                        name="button"
+                      >
+                        See Recipe
+                      </button>
+                      <input type="hidden" name="query" value="Pizza" />
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
 
 export default Menu;
+
+export const getStaticProps = async () => {
+  const response = await axios.get("/recipe.json");
+  const data = await response.data;
+  const recipes = [];
+  for (let key in data) {
+    recipes.push({
+      id: key,
+      data: data[key],
+    });
+  }
+  return {
+    props: {
+      recipes: [],
+    },
+    revalidate: 10,
+  };
+};
